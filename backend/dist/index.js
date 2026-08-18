7c35f1464b2f57f6cd3930ee197684f7f67166bd
Golfring <golfring.dove@hotmail.com>
2026-08-18 18:15:47 +0700

    test POST

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,33 +1,40 @@
 import { createServer } from "http";
+const book = [
+    { id: 1, name: "AAA", price: 200 },
+    { id: 2, name: "BBB", price: 300 },
+    { id: 3, name: "CCC", price: 400 }
+];
 const server = createServer((req, res) => {
     const parts = (req.url ?? "/").split("/"); //?? เช็คว่าว่างไหม เพราะถ้าไม่ทำ ts มันจะฟ้อง
-    if (req.method === "GET" && parts[1] === "book" && parts[2]) {
-        //res.end("Book ID: 8")
-        const book = [
-            { id: 1, name: "AAA", price: 200 },
-            { id: 2, name: "BBB", price: 300 },
-            { id: 3, name: "CCC", price: 400 }
-        ];
-        console.log("GET >> ", parts[1], " ID: ", parts[2]);
-        const id = Number(parts[2]);
-        if (Number.isNaN(id)) { //ควรตรวจสอบกรณีที่ไม่ได้ส่งมาเป็นเลข เช่น book/abc
-            res.statusCode = 400;
-            res.end("Invalid Book id :(");
-            return;
-        }
-        let show = null;
-        for (let i = 0; i < book.length; i++) {
-            const run = book[i];
-            if (run && run.id === id) { //จากเดิมใช้ if(book[i] && book[i].id === id) >> ts เรื่องมากอีก เพราะกลัวค่าว่าง ต่อให้ดักด้วย if book[i] แล้วก็ยังกลัว book[i].id ว่างอีก เลยต้องสร้าง run มาคอยรับค่าชั่วคราวแทน อื้อ กูยอมเองก็ได้ 555
-                show = run;
+    if (req.method === "GET" && parts[1] === "book") {
+        if (parts[2]) {
+            console.log("GET >> ", parts[1], " ID: ", parts[2]);
+            const id = Number(parts[2]);
+            if (Number.isNaN(id)) { //ควรตรวจสอบกรณีที่ไม่ได้ส่งมาเป็นเลข เช่น book/abc
+                res.statusCode = 400;
+                res.end("Invalid Book id :(");
+                return;
             }
+            const show = book.find((book) => book.id === id);
+            if (!show) { //จากเดิมเราใช้ show === null เพื่อดูว่าค่าว่างไหม แต่ ts เรื่องเยอะ เขาคำนึงถึง undefine ด้วย ซึ่งเป็นคนละเคส จึงต้องใช้ !show เพื่อตรวจสอบทั้ง null และ undefine
+                res.statusCode = 404;
+                res.end("No data");
+                return;
+            }
+            res.end(`Name : ${show.name}  Price : ${show.price}`);
         }
-        if (!show) { //จากเดิมเราใช้ show === null เพื่อดูว่าค่าว่างไหม แต่ ts เรื่องเยอะ เขาคำนึงถึง undefine ด้วย ซึ่งเป็นคนละเคส จึงต้องใช้ !show เพื่อตรวจสอบทั้ง null และ undefine
-            res.statusCode = 404;
-            res.end("No data");
-            return;
+        else {
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(book));
         }
-        res.end(`Name : ${show.name}  Price : ${show.price}`);
+        /*let show = null;
+        for(let i=0;i<book.length;i++){
+            const run = book[i]
+            if(run && run.id === id){ //จากเดิมใช้ if(book[i] && book[i].id === id) >> ts เรื่องมากอีก เพราะกลัวค่าว่าง ต่อให้ดักด้วย if book[i] แล้วก็ยังกลัว book[i].id ว่างอีก เลยต้องสร้าง run มาคอยรับค่าชั่วคราวแทน อื้อ กูยอมเองก็ได้ 555
+                show = run
+                break
+            }
+        }*/
     }
 });
 server.listen(3000);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,35 @@
 import { createServer } from "http";
 
-const server = createServer(
-    (req,res) => {
-        const parts = (req.url ?? "/").split("/") //?? เช็คว่าว่างไหม เพราะถ้าไม่ทำ ts มันจะฟ้อง
-        if(req.method === "GET" && parts[1] === "book" && parts[2]){
-
-            const book = [
+const book = [
                 {id:1, name:"AAA", price:200},
                 {id:2, name:"BBB", price:300},
                 {id:3, name:"CCC", price:400}
             ]
-            console.log("GET >> ", parts[1], " ID: ", parts[2])
-            const id = Number(parts[2])
-            if(Number.isNaN(id)){ //ควรตรวจสอบกรณีที่ไม่ได้ส่งมาเป็นเลข เช่น book/abc
-                res.statusCode = 400
-                res.end("Invalid Book id :(")
-                return
-            }
 
-            const show = book.find((book) => book.id === id)
+const server = createServer(
+    (req,res) => {
+        const parts = (req.url ?? "/").split("/") //?? เช็คว่าว่างไหม เพราะถ้าไม่ทำ ts มันจะฟ้อง
+    
+        if(req.method === "GET" && parts[1] === "book"){
+            if(parts[2]){
+                console.log("GET >> ", parts[1], " ID: ", parts[2])
+                const id = Number(parts[2])
+                if(Number.isNaN(id)){ //ควรตรวจสอบกรณีที่ไม่ได้ส่งมาเป็นเลข เช่น book/abc
+                    res.statusCode = 400
+                    res.end("Invalid Book id :(")
+                    return
+                }
+                const show = book.find((book) => book.id === id)
+                if(!show){ //จากเดิมเราใช้ show === null เพื่อดูว่าค่าว่างไหม แต่ ts เรื่องเยอะ เขาคำนึงถึง undefine ด้วย ซึ่งเป็นคนละเคส จึงต้องใช้ !show เพื่อตรวจสอบทั้ง null และ undefine
+                    res.statusCode = 404
+                    res.end("No data")
+                    return
+                }
+                res.end(`Name : ${show.name}  Price : ${show.price}`)
+            }else{
+                res.setHeader("Content-Type", "application/json")
+                res.end(JSON.stringify(book))
+            }
             /*let show = null;
             for(let i=0;i<book.length;i++){
                 const run = book[i]
@@ -27,13 +38,19 @@ const server = createServer(
                     break
                 }
             }*/
+        }
 
-            if(!show){ //จากเดิมเราใช้ show === null เพื่อดูว่าค่าว่างไหม แต่ ts เรื่องเยอะ เขาคำนึงถึง undefine ด้วย ซึ่งเป็นคนละเคส จึงต้องใช้ !show เพื่อตรวจสอบทั้ง null และ undefine
-                res.statusCode = 404
-                res.end("No data")
-                return
-            }
-            res.end(`Name : ${show.name}  Price : ${show.price}`)
+        if(req.method === "POST" && parts[1] === "book"){
+            let body = ""
+            req.on("data", (chunk) => {
+                body += chunk
+            })
+            req.on("end", () => {
+                const obj = JSON.parse(body)
+                book.push(obj)
+                const show2 = JSON.stringify(obj)
+                res.end(show2)
+            })
         }
     }
 )
